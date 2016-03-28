8a69aaebbbe88c71a2271d6cf4e4fad5e8974863
kokoko313 <drabada@gmail.com>
2016-03-28 21:40:03 +0300

    save contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,5 +3,15 @@ class ContactsController < ApplicationController
   end
 
   def create 
+    @contact=Contact.new(params_params)
+    @contact.save
   end
+
+  private
+
+  def params_params
+    params.require(:contact).permit(:email,:message)  
+  end
+
+
 end
