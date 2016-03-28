faf11981686ba2933ada708c95e1fb4c7fa7a89c
kokoko313 <drabada@gmail.com>
2016-03-28 22:51:55 +0300

    disp validation errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  resources :articles
-  resource :contacts, only: [:new, :create] 
+
+
+  resources :articles 
+  resource :contacts, only: [:new,:create], path_names: {:new => ''}
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
