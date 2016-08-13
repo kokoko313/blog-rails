eaed95c6f26d47c1f841c11bcebf48b3bd9a9521
kokoko313 <drabada@gmail.com>
2016-08-13 14:20:44 +0300

    add buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "home#index"
   get 'home/index'
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
 
-  resources :articles 
+  resources :articles do
+    resources :comments
+  end
+  
   resource :contacts, only: [:new,:create], path_names: {:new => ''}
 
 
